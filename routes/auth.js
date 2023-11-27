d72330911b4a5003e556de67c9a4751a0fcbc67a
Marko Sudar <mjsudar21@catawba.edu>
2023-11-27 01:51:15 -0500

    Password reset finished

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,13 @@ const authController = require('../controllers/auth');
 router.get('/login', authController.getLogin);
 router.get('/register', authController.getRegister);
 router.get('/reset', authController.getReset);
+router.get('/reset/:token', authController.getNewPassword);
 
 // Post routes
 router.post('/login', authController.postLogin);
 router.post('/register', authController.postRegister);
 router.post('/logout', authController.postLogout);
+router.post('/reset', authController.postReset);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
